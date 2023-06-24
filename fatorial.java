cb3f30b2de2e1a82699d08d4c2e0512d314e324a
Nathan Medeiros Cristiano <nathan_cristiano@estudante.sesisenai.org.br>
2023-06-24 16:15:38 -0300

    Create fatorial.java

---
+++ fatorial.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;   
+
+public class Fatorial{
+    public int calc (int fatorial){
+        int resultado = fatorial;
+
+        if(fatorial==0 || fatorial==1){
+            return 1;            
+        }
+        if(fatorial<0){
+            System.out.println("não existe fatorial de numero negativo");
+        }        
+        else if(fatorial >1){
+                for(int i=fatorial-1;i>1;i--){
+                    resultado=resultado*i;
+            }
+        }       
+
+        return resultado;
+    }
+
+    public static void main(String[] args) {        
+
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Digite um numero para calculo do fatorial: ");
+        int numero=teclado.nextInt();        
+        
+        Fatorial ex = new Fatorial();
+        int resultado = ex.calc(numero);
+        
+        System.out.println(numero + "! = " + resultado);
+        }
+    }
